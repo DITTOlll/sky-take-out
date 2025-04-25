9bc8a2a26ee51a89b2238ba61e82a617135fdc0f
DITTOlll <1018465339@qq.com>
2025-04-25 10:41:04 +0800

    套餐分页查询功能业务开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/admin/setmeal")
+public class SetmealController {
+    @Autowired
+    SetmealService setmealService;
+    @GetMapping("/page")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO){
+        log.info("套餐分页查询:{}",setmealPageQueryDTO);
+        PageResult pageResult = setmealService.page(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+}
+    /*@GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询:{}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }*/
