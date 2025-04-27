4bc4fae35209f3b3d22e979911b17ba8dc14fed7
DITTOlll <1018465339@qq.com>
2025-04-27 15:25:35 +0800

    缓存套餐业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import com.sky.service.SetmealService;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +35,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(cacheNames = "setmealCache",key="#setmealDTO.categoryId")//key:setmealCache::100
     public Result save(@RequestBody SetmealDTO setmealDTO){
         setmealService.setWithDish(setmealDTO);
         return Result.success();
@@ -45,6 +47,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         log.info("删除套餐:{}",ids);
         setmealService.deleteBatch(ids);
@@ -66,6 +69,7 @@ public class SetmealController {
      * 修改套餐
      */
     @PutMapping
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody  SetmealDTO setmealDTO){
         log.info("修改套餐:{}",setmealDTO);
         setmealService.update(setmealDTO);
@@ -73,6 +77,7 @@ public class SetmealController {
     }
 
     @PostMapping("/status/{status}")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status,Long id){
         log.info("启用禁用套餐:{},{}",status,id);
         setmealService.startOrStop(status,id);
--- sky-server/src/main/java/com/sky/controller/user/UserController.java
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -11,6 +11,7 @@ import com.sky.vo.UserLoginVO;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
