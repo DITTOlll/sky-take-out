90d7141c6fd03127379c6d8a8059c140f5d893c9
DITTOlll <1018465339@qq.com>
2025-04-27 16:22:24 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,7 +1,17 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 public interface ShoppingCartService {
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 购物车展示
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
+
 }
